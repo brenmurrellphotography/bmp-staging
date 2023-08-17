58ac49808e772a9f653d2e377dd3eede8e55f143
Bren Murrell <bren@devacademy.co.nz>
2023-08-17 17:23:24 +1200

    add galleries

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import { Link, type HeadFC, type PageProps } from "gatsby"
 import { Banner } from "../components/Banner"
 
 import banner1 from "../images/banner-1.jpg"
 import banner2 from "../images/banner-2.jpg"
 import banner3 from "../images/banner-3.jpg"
 import banner4 from "../images/banner-4.jpg"
+
+import brenMurrell from "../images/gallery/headshot.jpg"
 
 const docLinks = [
   {
@@ -79,8 +81,26 @@ const IndexPage: React.FC<PageProps> = () => {
       <Banner images={images} />
       <main>
         <h1>Welcome!</h1>
-        <p>Hi, I'm Bren and I'm a photographer based in Tauwharenikau, New Zealand.</p>
-        <p>This is my new site and I'm working on getting it ready to accept bookings and sell my art prints. Please stand by!</p>
+        <div className="simple-grid">
+          <div className="simple-grid__fill">
+              <p>Hi, I'm Bren and I'm a photographer based in Tauwharenikau, New Zealand.</p>
+              <p>This is my new site and I'm working on getting it ready to accept bookings and sell my art prints. Please stand by!</p>
+              <p>
+                Take a look at our <Link to="/gallery">gallery</Link> page. If you
+                see something you like, give us a&nbsp;
+                <a href="tel:+64274670548">call</a>&nbsp;
+                or&nbsp;
+                <a href="sms:+64274670548?body=I'm interested in one of your art prints">
+                  text
+                </a>&nbsp;
+                to arrange a booking or an art print
+                today - or visit at us on <a href="https://facebook.com/brenmurrellphotography">Facebook</a>.
+              </p>
+          </div>
+          <div className="simple-grid__400">
+            <img src={brenMurrell} className="rounded" alt="Bren Murrell - low key headshot image of photographer" />
+          </div>
+        </div>
         {/* <ul>
           {docLinks.map(doc => (
             <li key={doc.url} >
